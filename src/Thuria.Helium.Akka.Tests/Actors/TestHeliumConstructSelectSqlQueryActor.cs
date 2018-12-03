560a43b60e30d0c7b9f3884c897af74e51c47729
Johan Dercksen <johan@draco.co.za>
2018-12-03 16:00:43 +0200

    Added unit tests for Helium Actors

--- src/Thuria.Helium.Akka.Tests/Actors/TestHeliumConstructSelectSqlQueryActor.cs
+++ src/Thuria.Helium.Akka.Tests/Actors/TestHeliumConstructSelectSqlQueryActor.cs
@@ -7,7 +7,10 @@ using FluentAssertions;
 using Akka.TestKit.NUnit3;
 
 using Thuria.Helium.Akka.Actors;
+using Thuria.Helium.Akka.Messages;
+using Thuria.Helium.Core;
 using Thuria.Thark.Core.Statement.Builders;
+using Thuria.Thark.StatementBuilder.Builders;
 
 namespace Thuria.Helium.Akka.Tests.Actors
 {
@@ -55,6 +58,60 @@ namespace Thuria.Helium.Akka.Tests.Actors
       EventFilter.Exception(typeof(ArgumentNullException), contains: "Parameter name: conditionBuilder", checkInnerExceptions: true)
                  .Expect(1, () => Sys.ActorOf(actorProps, "Test"));
       //---------------Test Result -----------------------
+    }
+
+    [Test]
+    public void HeliumConstructSqlQueryMessage_GivenHeliumActionNotRetrieve_ShouldNotHandleMessage()
+    {
+      //---------------Set up test pack-------------------
+      var actorRef        = CreateActor();
+      var sqlQueryMessage = new HeliumConstructSqlQueryMessage(HeliumAction.Insert, new HeliumFakeDataModel { Id = Guid.NewGuid() });
+      //---------------Assert Precondition----------------
+      //---------------Execute Test ----------------------
+      actorRef.Tell(sqlQueryMessage);
+      //---------------Test Result -----------------------
+      ExpectNoMsg();
+    }
+
+    [Test]
+    public void HeliumConstructSqlQueryMessage_GivenValidMessage_ShouldHandleMessage()
+    {
+      //---------------Set up test pack-------------------
+      var actorRef        = CreateActor();
+      var sqlQueryMessage = new HeliumConstructSqlQueryMessage(HeliumAction.Retrieve, new HeliumFakeDataModel { Id = Guid.NewGuid() });
+      //---------------Assert Precondition----------------
+      //---------------Execute Test ----------------------
+      actorRef.Tell(sqlQueryMessage);
+      //---------------Test Result -----------------------
+      var resultMessage = ExpectMsg<HeliumConstructSqlQueryResultMessage>();
+      resultMessage.Should().NotBeNull();
+    }
+
+    [Test]
+    public void HeliumConstructSqlQueryMessage_GivenValidMessage_ShouldHandleMessageAndReturnExpectedSqlQuery()
+    {
+      //---------------Set up test pack-------------------
+      var dataModelId      = Guid.NewGuid();
+      var expectedSqlQuery = $"SELECT [HeliumFake].[Id], [HeliumFake].[Description], [HeliumFake].[IsActive] FROM [HeliumFake] WHERE  [HeliumFake].[Id] = '{dataModelId}' ";
+      var actorRef         = CreateActor(SelectStatementBuilder.Create, ConditionBuilder.Create);
+      var sqlQueryMessage  = new HeliumConstructSqlQueryMessage(HeliumAction.Retrieve, new HeliumFakeDataModel { Id = dataModelId });
+      //---------------Assert Precondition----------------
+      //---------------Execute Test ----------------------
+      actorRef.Tell(sqlQueryMessage);
+      //---------------Test Result -----------------------
+      var resultMessage = ExpectMsg<HeliumConstructSqlQueryResultMessage>();
+      resultMessage.Should().NotBeNull();
+      resultMessage.HeliumAction.Should().Be(sqlQueryMessage.HeliumAction);
+      resultMessage.SqlQuery.Should().Be(expectedSqlQuery);
+    }
+
+    private IActorRef CreateActor(ISelectStatementBuilder statementBuilder = null, IConditionBuilder conditionBuilder = null)
+    {
+      var selectStatementBuilder = statementBuilder ?? Substitute.For<ISelectStatementBuilder>();
+      var selectConditionBuilder = conditionBuilder ?? Substitute.For<IConditionBuilder>();
+
+      var actorProps = Props.Create<HeliumConstructSelectSqlQueryActor>(selectStatementBuilder, conditionBuilder);
+      return Sys.ActorOf(actorProps, "Test");
     }
   }
 }
---
+++ src/Thuria.Helium.Akka.Tests/HeliumFakeDataModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Thuria.Helium.Akka.Tests
+{
+  public class HeliumFakeDataModel
+  {
+    public Guid Id { get; set; }
+    public string Description { get; set; }
+    public bool IsActive { get; set; }
+  }
+}
